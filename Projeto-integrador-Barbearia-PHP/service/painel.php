7798329e3445922acdcdbf3f3d50a3647ef2bab2
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2024-03-11 03:24:05 -0300

    Atualizando!

--- Projeto-integrador-Barbearia-PHP/service/painel.php
+++ Projeto-integrador-Barbearia-PHP/service/painel.php
@@ -1,6 +1,23 @@
 <?php
 include('../domain/protect.php');
 
+ // require ('./data/site_db.php');
+    $hora = date(format: "h:i");
+
+    $numero_hora =date(format: "H");
+    $mensagem = "Bom Dia";
+    if($numero_hora >= 12){
+        $mensagem = "Boa tarde";
+    } 
+    if($numero_hora >= 18){
+        $mensagem = "Boa noite";
+    }
+
+    $numero_hora = date("H");
+    $mensagem = "Bom dia";
+    if ($numero_hora >= 12) $mensagem = "Boa tarde";
+    if ($numero_hora >= 18) $mensagem = "Boa noite";
+
 ?>
 
 <!DOCTYPE html>
@@ -13,8 +30,8 @@ include('../domain/protect.php');
 </head>
 
 <body>
-    Bem vindo ao Projeto Integrador do Grupo 26 , <?php echo $_SESSION['nome']; ?>
-
+    Bem vindo ao Projeto Integrador do Grupo 26 , <?php echo $mensagem?>, <?php echo $_SESSION['nome']; ?>
+    
     <p>
         <a href="../domain/login.php">Sair</a>
     </p>
